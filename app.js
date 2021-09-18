f184badd04b0d523bbb7f810bb2fdaff76796e43
Humaiun-Manik <humaiunkobirr@gmail.com>
2021-09-18 23:13:29 +0600

    display result

--- app.js
+++ app.js
@@ -9,9 +9,11 @@ searchBtn.addEventListener('click', function () {
     fetch(url)
         .then(res => res.json())
         .then((data) => {
+            // show search result
+            const totalResult = data.docs.length;
+            document.getElementById('search-result').innerText = totalResult;
+            // display search books
             (data.docs).forEach((items) => {
-                console.log(items);
-                document.getElementById('search-result').innerText = `${items.count}`
                 const div = document.createElement('div');
                 div.classList.add('col');
                 div.innerHTML = `
@@ -29,13 +31,3 @@ searchBtn.addEventListener('click', function () {
             });
         });
 });
-
-{/* <div class="card shadow p-3 mb-5 bg-body rounded">
-    <img src="${`https://covers.openlibrary.org/b/id/${items.cover_i}-M.jpg`}" class="card-img-top w-100" alt="...">
-    <div class ="card-body">
-    <h3 class ="card-title">${items.title}</h3>
-    <h5 class ="card-title">${items.author_name}</h5>
-    <p class ="card-text">This is a longer card with supporting text below as a natural lead-in to
-    additional content.This content is a little bit longer.</p>
-    </div>
-</div> */}
